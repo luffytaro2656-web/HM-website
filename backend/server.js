49b3fa5307e61fe7c958438e1e79d252705ce011
Msuhash <msuhashrdj@gmail.com>
2026-06-18 22:04:10 +0530

    changed

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,5 @@ const app = express();
 app.use(cors({ origin: 'http://localhost:5173' })); // Vite default port
 app.use(json());
 
-
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
